1b2796b554195401c874c69fe5e32af704703fdf
gcooney29 <gcooney@colgate.edu>
2019-06-24 12:19:04 -0400

    created matrix

--- cores_organizer.R
+++ cores_organizer.R
@@ -1,2 +1,16 @@
 #script for organizing hemlock core data
-#comment
+
+#set working directory
+setwd("~/Desktop/hemlockdata")
+
+#read in data
+datT1 <- read.csv ("datT1.csv")
+datT2 <- read.csv ("datT2.csv")
+datT3 <- read.csv ("datT3.csv")
+
+#create 15x73 matrix
+meanring <- matrix(NA, dim(datT1[1]), ncol = dim(datT1[1]),
+                   nrow = dim(datT1[2]))
+
+
+
