ab6f9ea8cc43898003728c779c1889de9e945b00
gcooney29 <gcooney@colgate.edu>
2019-06-24 14:19:09 -0400

    fixed matrix and calculated means

--- cores_organizer.R
+++ cores_organizer.R
@@ -8,9 +8,12 @@ datT1 <- read.csv ("datT1.csv")
 datT2 <- read.csv ("datT2.csv")
 datT3 <- read.csv ("datT3.csv")
 
-#create 15x73 matrix
-meanring <- matrix(NA, dim(datT1[1]), ncol = dim(datT1[1]),
-                   nrow = dim(datT1[2]))
+#create matrix
+meanring <- matrix(rep(NA, dim(datT1)[1]*(dim(datT1)[2]-1)),
+                   nrow = dim(datT1)[1])
 
-
+#calculate means across 3 datasets
+for(i in 1:(ncol(datT1)-1)){
+  meanring[,i] <- (datT1[,i+1] + datT2[,i+1] + datT3[,i+1])/3
+}
 
